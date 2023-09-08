dd71f2f48369060d00cc172a28cb05d191fa5f49
Kalyani <37569003+kalyani33@users.noreply.github.com>
2023-09-08 17:48:48 +0530

    Delete ems-backend directory

--- ems-backend/src/main/java/com/example/ems/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ems.entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-
-}
